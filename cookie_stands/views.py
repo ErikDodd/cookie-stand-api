147d757ebafa11d68d9b5f68a6d6cf0b56e3ec7f
Erik M. Dodd <emdodd27@gmail.com>
2023-02-02 19:27:06 -0800

    Basically done with lab 34. Need to add ReadME and double check all the CRUD template pages

--- cookie_stands/views.py
+++ cookie_stands/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from .models import CookieStand
+from .serializer import CookieStandSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class CookieStandList(ListCreateAPIView):
+    queryset = CookieStand.objects.all()
+    serializer_class = CookieStandSerializer
+
+
+class CookieStandDetail(RetrieveUpdateDestroyAPIView):
+    queryset = CookieStand.objects.all()
+    permission_classes = (IsOwnerOrReadOnly,)
+    serializer_class = CookieStandSerializer
--- cookie_standsssss/admin.py
+++ cookie_standsssss/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Thing
 
 # Register your models here.
-
